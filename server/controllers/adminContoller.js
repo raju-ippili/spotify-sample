2f8a534e33b9bfec9ebd4318f98d5a1334ec6665
AkhilG3 <akhilganthala@gmail.com>
2025-09-12 17:57:41 +0530

    update

--- server/controllers/adminContoller.js
+++ server/controllers/adminContoller.js
@@ -1,32 +1,36 @@
-const prisma=require('../utils/ConnectDB')
-require('dotenv').config();
-exports.adminLogin=async(req,res)=>{
-    try {
-        const user= await prisma.user.findMany({
-        select: {
-        id: true,
-        name: true,
-        email: true,
-        role: true,
-        createdAt: true
-      }
-        })
-    res.status(200).send({message:"found"})
-    } catch (err) {
-        res.status(500).json({ message: "Server error", error: error.message });
+import express from "express";
+import prisma from "../utils/ConnectDB.js";
+import dotenv from "dotenv";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+export const adminLogin = async (req, res) => {
+  const { email, pass } = req.body;
+
+  try {
+    const validUser = await prisma.user.findFirst({
+      where: { email, role: "ADMIN" },
+    });
+
+    if (!validUser) {
+      return res.status(400).json({ message: "User doesn't exist" });
     }
-}
-exports.getUsers = async(req,res)=>{
-    const {id}=req.params;
-    try{
-        const user = await prisma.user.findUnique({
-            where:{id:id},
-        });
-        if(!user){
-        res.status(400).send({message:"User not found"})
-        }
-        res.status(200).send(user);
-    }catch(err){
-        res.status(500).json({message:"server error",error:err.message});
+
+    const validPass = await bcrypt.compare(pass, validUser.password);
+    if (!validPass) {
+      return res.status(400).json({ message: "Wrong password" });
     }
-}
+
+    const token = jwt.sign(
+      { id: validUser.id, email: validUser.email, role: validUser.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "6h" }
+    );
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err.message });
+  }
+};
