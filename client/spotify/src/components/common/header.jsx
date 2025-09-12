8c848c808559dbb4d11edc724e668baa4de41e26
AkhilG3 <akhilganthala@gmail.com>
2025-09-12 18:03:23 +0530

    new update

--- client/spotify/src/components/common/header.jsx
+++ client/spotify/src/components/common/header.jsx
@@ -1,10 +1,10 @@
-import Logo from "./assets/logo.png"
+import Logo from "../../assets/logo.png"
 export default function Header(){
 return(
     <div className="bg-gray-950 px-3 py-3">
         <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
-            <img src={Logo} alt="logo" className="h-6 w-6"/>
+            <img src={Logo} alt="logo" className="h-15 w-15"/>
             <h1 className="text-white text-[25px] font-bold ">Spotify</h1>
         </div>
         </div>
